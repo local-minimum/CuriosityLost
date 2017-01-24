ae162262e045dfdc9f7b80ebf4df13d6bcbc125b
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-01-24 22:46:58 +0100

    :star: Added zoom to spaceship

---
+++ Assets/Scripts/CamZoomToShip.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamZoomToShip : MonoBehaviour {
+
+    [SerializeField]
+    Transform spaceShip;
+
+    [SerializeField]
+    AnimationCurve zoomCurve;
+
+    [SerializeField, Range(0, 2)]
+    float duration = 1f;
+
+    [SerializeField]
+    Vector3 targetOffset = Vector3.up * 7f + Vector3.left * 4f;
+
+    [SerializeField]
+    float preZoomPause = 0.25f;
+
+	void Start () {
+		ZoomIn();
+	}
+
+    public void ZoomIn()
+    {
+        StartCoroutine(zoom());
+    }	
+
+    IEnumerator<WaitForSeconds> zoom()
+    {
+        yield return new WaitForSeconds(preZoomPause);
+
+        float start = Time.timeSinceLevelLoad;
+        float delta = 0;
+
+        Vector3 origin = transform.position;
+
+        while (delta < 1)
+        {
+            transform.position = Vector3.LerpUnclamped(origin, spaceShip.position + targetOffset, zoomCurve.Evaluate(delta));
+            yield return new WaitForSeconds(0.016f);
+            delta = Mathf.Min(1, (Time.timeSinceLevelLoad - start) / duration);
+        }
+
+        transform.position = spaceShip.position + targetOffset;
+    }
+}
