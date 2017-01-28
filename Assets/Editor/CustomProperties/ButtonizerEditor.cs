63ac9dba03ddbfa0c3522b5106cc152f8d3d0b33
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-01-28 11:26:22 +0100

    :wrench: Testing custom scripts

---
+++ Assets/Editor/CustomProperties/ButtonizerEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomPropertyDrawer(typeof(Buttonizer))]
+public class ButtonizerEditor : PropertyDrawer {
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        string buttonText = (this.fieldInfo.GetCustomAttributes(false)[0] as Buttonizer).name;
+
+        if (GUI.Button(position, string.IsNullOrEmpty(buttonText) ? property.name : buttonText))
+        {
+            MonoBehaviour script = property.serializedObject.targetObject as MonoBehaviour;
+            script.SendMessage(buttonText, SendMessageOptions.RequireReceiver);
+        }
+        
+    }
+}
