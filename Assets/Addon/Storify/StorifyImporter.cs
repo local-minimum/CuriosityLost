6dbd570ead503d6d3b9478d17a4460c1fea2681c
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-03-05 13:27:33 +0100

    Started on storify importer

--- Assets/Addon/Storify/StorifyImporter.cs
+++ Assets/Addon/Storify/StorifyImporter.cs
@@ -3,104 +3,159 @@ using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using UnityEngine;
 
-public class StorifyImporter : MonoBehaviour {
+namespace LM_Storify
+{
 
-    [SerializeField]
-    string importPath;
-
-    private void Start()
+    public struct StorifyOption
     {
-        if (!string.IsNullOrEmpty(importPath)) {
-            LoadFromPath(importPath);
+        public string text;
+        public StorifyLogic logic;
+
+        public override string ToString()
+        {
+            return text + "{" + logic + "}";
+        }
+
+        public StorifyOption(string text, StorifyLogic logic)
+        {
+            this.text = text;
+            this.logic = logic;
+        }
+
+        public StorifyOption(string text)
+        {
+            this.text = text;
+            logic = null;
         }
     }
 
-    public void LoadFromPath(string path)
+    public class StorifyImporter : MonoBehaviour
     {
-        foreach (TextAsset ta in Resources.LoadAll<TextAsset>(path))
+
+        [SerializeField]
+        string importPath;
+
+        private void Start()
         {
-            var classifier = ParseTextAssetName(ta);
+            if (!string.IsNullOrEmpty(importPath))
+            {
+                LoadFromPath(importPath);
+            }
+        }
+
+        public void LoadFromPath(string path)
+        {
+            foreach (TextAsset ta in Resources.LoadAll<TextAsset>(path))
+            {
+                var classifier = ParseTextAssetName(ta);
+
+                switch (classifier.Value)
+                {
+                    case "story":
+                        ParseStory(ta);
+                        break;
+                    case "data":
+                        break;
+                    default:
+                        Debug.LogError(string.Format("Resource '{0}' in '{1}' not understood type '{2}'.",
+                            ta.name, path, classifier.Value));
+                        break;
+
+                }
+            }
+        }
+
+        KeyValuePair<string, string> ParseTextAssetName(TextAsset ta)
+        {
+
+            int divider = ta.name.LastIndexOf('.');
+            return new KeyValuePair<string, string>(
+                ta.name.Substring(0, divider),
+                ta.name.Substring(divider + 1));
+        }
+
+        void ParseStory(TextAsset ta)
+        {
+            string[] lines = ta.text.Split(new char[] { '\n', '\r' });
+            for (int i=0,l=lines.Length; i< l; i++)
+            {
+                string line = lines[i].Trim();
+
+                if (!string.IsNullOrEmpty(line))
+                {
+                    ParseStoryBit(line);
+                }
+            }
+        }
+
+        Regex sentenceRegEx = new Regex("\\[([^\\]]+)]");
+
+        void ParseStoryBit(string line)
+        {
             
-            switch (classifier.Value)
+            string processed = "";
+            int from = 0;
+            int index = 0;
+            var match = sentenceRegEx.Match(line);
+            var optionsList = new List<List<StorifyOption>>();
+            while (match.Success)
+            {                
+                processed += line.Substring(from, match.Index - from);
+                processed += string.Format("[{0}]", index);
+                from = match.Index + match.Length;
+
+                optionsList.Add(ParseOptions(match.Groups[1].Value));
+
+                match = match.NextMatch();
+                index++;
+            }
+            processed += line.Substring(from);
+            Debug.Log("Proecessed line: " + processed);
+
+        }
+
+        Regex optionRegEx = new Regex("^([^\\{]+)(\\{[^\\}]*\\})?$");
+
+        List<StorifyOption> ParseOptions(string options)
+        {
+            var parsedOptions = new List<StorifyOption>();
+
+            foreach (string option in options.Split('|'))
             {
-                case "story":
-                    ParseStory(ta);
-                    break;
-                case "data":
-                    break;
-                default:
-                    Debug.LogError(string.Format("Resource '{0}' in '{1}' not understood type '{2}'.",
-                        ta.name, path, classifier.Value));
-                    break;
+
+                if (string.IsNullOrEmpty(option.Trim()))
+                {
+                    continue;
+                }
+
+                var match = optionRegEx.Match(option);
+
+                //Debug.Log(string.Format("Story option groups ({0})", string.Join(" :: ", match.Groups.Cast<Group>().Select(e => e.Value).ToArray())));
+
+                string optionText = match.Groups[1].Value;
+                string optionLogic = match.Groups[2].Value.Trim();
+
+                if (string.IsNullOrEmpty(optionText))
+                {
+                    Debug.LogError(string.Format("Malformed option '{0}'", option));
+                }
+                if (string.IsNullOrEmpty(optionLogic))
+                {
+                    parsedOptions.Add(new StorifyOption(optionText));
+                }
+                else
+                {                    
+                    parsedOptions.Add(new StorifyOption(
+                        optionText,
+                        StorifyLogic.CreateFromString(optionLogic.Substring(1, optionLogic.Length - 2))));
+                }
+
+                //Debug.Log(parsedOptions[parsedOptions.Count - 1]);
 
             }
+            return parsedOptions;
         }
+
     }
 
-    KeyValuePair<string, string> ParseTextAssetName(TextAsset ta)
-    {
-
-        int divider = ta.name.LastIndexOf('.');
-        return new KeyValuePair<string, string>(
-            ta.name.Substring(0, divider),
-            ta.name.Substring(divider + 1));
-    }
-
-    void ParseStory(TextAsset ta)
-    {
-        foreach(string line in ta.text.Split('\n'))
-        {
-            ParseStoryBit(line.Trim());
-        }
-    }
-
-    string optionsPattern = "\\[([^\\]]+)\\]";
-
-    void ParseStoryBit(string line)
-    {
-        string processed = "";
-        int from = 0;
-        var matches = Regex.Matches(line, optionsPattern);
-        for (int i=0; i<matches.Count; i++)
-        {
-            var match = matches[i];
-            processed += line.Substring(from, match.Index - from);
-            processed += string.Format("[{0}]", i);
-            from = match.Index + match.Length;
-
-            ParseOptions(matches[i].Groups[0].Value.Substring(1, match.Groups[0].Length - 2));
-        }
-        processed += line.Substring(from);
-        Debug.Log(processed);
-        
-    }
-
-    string optionPattern = "([^<]+)(<[^>]>)?";
-
-    List<KeyValuePair<string, KeyValuePair<string, int>>> ParseOptions(string options)
-    {
-        var parsedOptions = new List<KeyValuePair<string, KeyValuePair<string, int>>>();
-
-        foreach(string option in options.Split('|'))
-        {
-            var match = Regex.Match(option, optionPattern);
-            if (match.Groups.Count == 1)
-            {
-                parsedOptions.Add(new KeyValuePair<string, KeyValuePair<string, int>>(match.Groups[0].Value, new KeyValuePair<string, int>()));
-            } else if (match.Groups.Count == 2)
-            {
-                parsedOptions.Add(new KeyValuePair<string, KeyValuePair<string, int>>(
-                    match.Groups[0].Value,
-                    ParseEffect(match.Groups[1].Value.Substring(1, match.Groups[1].Value.Length - 2))));
-            }
-            Debug.Log(match.Groups[0].Value);
-        }
-        return parsedOptions;
-    }
-
-    KeyValuePair<string, int> ParseEffect(string optionEffect)
-    {
-        return new KeyValuePair<string, int>();
-    }
 }
-
---
+++ Assets/Addon/Storify/StorifyLogic.cs
@@ -0,0 +1,222 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace LM_Storify {
+
+    public enum EffectLogic {Absolute, Relative}
+
+    public struct StorifyEffect
+    {
+        public string effectName;
+        public int amount;
+        public EffectLogic logic;
+
+        public override string ToString()
+        {
+            return string.Format("{2}{0} {1}", Mathf.Abs(amount), effectName, logic == EffectLogic.Absolute ? "" : (amount < 0 ? "-" : "+"));
+        }
+    }
+
+    public enum RequirementLogic { LessThan, GreaterThan, EqualTo};
+
+    public struct StorifyRequirement
+    {
+        public string requirementName;
+        public int amount;
+        public RequirementLogic logic;
+
+        public override string ToString()
+        {
+            return string.Format("{0} {2} {1}", requirementName, amount, "<>="[(int)logic]);
+        }
+    }
+
+    [System.Serializable]
+    public class StorifyLogic {
+
+        public List<StorifyEffect> effects = new List<StorifyEffect>();
+        public List<StorifyRequirement> requirements = new List<StorifyRequirement>();
+
+        public override string ToString()
+        {
+            string s1 = string.Join(",", effects.Select(e => e.ToString()).ToArray());
+            string s2 = string.Join(",", requirements.Select(e => e.ToString()).ToArray());
+            if (string.IsNullOrEmpty(s1))
+            {
+                return s2;
+            } else if (string.IsNullOrEmpty(s2))
+            {
+                return s1;
+            } else
+            {
+                return s1 + "," + s2;
+            }            
+        }
+
+        public bool Validate(string[] characterStatNames)
+        {
+            HashSet<string> invalids = new HashSet<string>();
+
+            for (int i = 0, l = requirements.Count; i < l; i++)
+            {
+                if (!characterStatNames.Contains(requirements[i].requirementName))
+                {
+                    invalids.Add(requirements[i].requirementName);
+                }
+            }
+
+            for (int i = 0, l = effects.Count; i < l; i++)
+            {
+                if (!characterStatNames.Contains(effects[i].effectName))
+                {
+                    invalids.Add(effects[i].effectName);
+                }
+            }
+
+            if (invalids.Count != 0)
+            {
+                Debug.LogError(string.Format("StorifyLogic '{0}' requires unsuppored character stats {1}", this, invalids.ToArray()));
+                return false;
+            } else
+            {
+                return true;
+            }
+            
+        }
+
+        public bool IsAllowed(System.Func<string, int> characterStatsGetter)
+        {
+            for (int i=0, l=requirements.Count; i< l; i++)
+            {
+                if (!IsAllowed(characterStatsGetter(requirements[i].requirementName), requirements[i]))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        public static bool IsAllowed(int value, StorifyRequirement requirement)
+        {
+            switch (requirement.logic)
+            {
+                case RequirementLogic.EqualTo:
+                    return value == requirement.amount;
+                case RequirementLogic.LessThan:
+                    return value < requirement.amount;
+                case RequirementLogic.GreaterThan:
+                    return value > requirement.amount;
+                default:
+                    return false;
+            }
+        }
+
+        public void InvokeEffects(System.Action<string, int> characterStatsSetterRelative, System.Action<string, int> characterStatsSetterAbsolute)
+        {
+            for (int i = 0, l = effects.Count; i < l; i++)
+            {
+                switch (effects[i].logic)
+                {
+                    case EffectLogic.Absolute:
+                        characterStatsSetterAbsolute(effects[i].effectName, effects[i].amount);
+                        break;
+                    case EffectLogic.Relative:
+                        characterStatsSetterRelative(effects[i].effectName, effects[i].amount);
+                        break;
+                    default:
+                        Debug.LogWarning(string.Format("Can't invoke effect '{0}' because missing logic", effects[i]));
+                        break;
+                }
+            }
+        }
+
+        public static StorifyLogic CreateFromString(string s)
+        {
+            StorifyLogic item = new StorifyLogic();
+            item.parseSet(s);
+            return item;
+        }
+
+        void parseSet(string text)
+        {
+            string[] instructions = text.Split(',');
+
+            for (int i = 0, l = instructions.Length; i < l; i++)
+            {
+                string instruction = instructions[i].Trim();
+                StorifyEffect effect;
+                StorifyRequirement requirement;
+
+                if (string.IsNullOrEmpty(instruction))
+                {
+                    continue;
+                } else if (IsEffect(instruction, out effect))
+                {
+                    effects.Add(effect);
+                } else if (IsRequirement(instruction, out requirement))
+                {
+                    requirements.Add(requirement);
+                } else
+                {
+                    Debug.LogError(string.Format("StorifyLogic could not parse '{0}' in '{1}'", instruction, text));
+                }  
+            }
+        }
+        
+        Regex effectRegEx = new Regex("^([+-]?\\d+) ?([\\w _-]+)$");
+
+        bool IsEffect(string text, out StorifyEffect effect)
+        {
+            effect = new StorifyEffect();
+
+            var match = effectRegEx.Match(text);
+            string amount = match.Groups[1].Value.Trim();
+            string characterStat = match.Groups[2].Value.Trim();
+
+            if (string.IsNullOrEmpty(characterStat) || string.IsNullOrEmpty(amount))
+            {
+                return false;
+            } else 
+            {
+
+                //Debug.Log(amount + " :: " + characterStat);
+                effect.effectName = characterStat;
+                effect.logic = amount.IndexOfAny(new char[] { '+', '-' }) == 0 ? EffectLogic.Relative : EffectLogic.Absolute;
+                effect.amount = int.Parse(amount.StartsWith("+") ? amount.Substring(1) : amount);
+
+                return true;
+            }
+        }
+
+
+        Regex requirementRegEx = new Regex("^(\\w+) ?([><=]) ?([+-]?\\d)$");
+
+        bool IsRequirement(string text, out StorifyRequirement requirement)
+        {
+            requirement = new StorifyRequirement();
+
+            var match = requirementRegEx.Match(text);
+            string characterStat = match.Groups[1].Value.Trim();
+            string logic = match.Groups[2].Value.Trim();
+            string amount = match.Groups[3].Value.Trim();
+
+            if (string.IsNullOrEmpty(characterStat) || string.IsNullOrEmpty(logic) || string.IsNullOrEmpty(amount))
+            {
+                return false;
+            } else
+            {
+
+                requirement.requirementName = characterStat;
+                requirement.logic = (RequirementLogic)"<>=".IndexOf(logic);
+                requirement.amount = int.Parse(amount.StartsWith("+") ? amount.Substring(1) : amount);
+                
+                return true;
+            }
+
+        }
+    }
+
+}
